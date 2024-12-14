023320e20be4eefcfefd37cef79d13a7e084e83f
MOe <m.aljaaly@yahoo.co.uk>
2024-12-14 02:32:06 +0000

    up2

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '526b3d132c98d2753e36ca720e24a7a1c0e2814d', queries,  });
+export default client;
+  
