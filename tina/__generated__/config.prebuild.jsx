cf495f42f857cc3820130f2beae3ae7ec2b75b1b
MOe <m.aljaaly@yahoo.co.uk>
2024-12-14 02:03:05 +0000

    upercel

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,77 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+import slugify from "slugify";
+var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "master";
+var config_default = defineConfig({
+  client: {
+    // Replace with your TinaCMS GraphQL API URL
+    apiUrl: process.env.NEXT_PUBLIC_TINA_URL || "http://localhost:4001/graphql"
+  },
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        label: "Blog Posts",
+        name: "posts",
+        path: "app/blog/content/posts",
+        format: "md",
+        ui: {
+          filename: {
+            // Generate SEO-friendly filenames based on the title
+            slugify: (values) => {
+              return slugify(values.title || "untitled", {
+                lower: true,
+                strict: true
+              });
+            }
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            label: "Author",
+            name: "author",
+            required: true
+          },
+          {
+            type: "datetime",
+            label: "Date",
+            name: "date",
+            required: true
+          },
+          {
+            type: "rich-text",
+            label: "Content",
+            name: "body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
