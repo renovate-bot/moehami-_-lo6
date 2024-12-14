cf8987559834f6f3c9e0cee35cf8fc6a1996a012
moehami <m.aljaaly@yahoo.co.uk>
2024-12-14 12:47:19 +0200

    Update next.d.ts

--- next.d.ts
+++ next.d.ts
@@ -1,7 +1,6 @@
-// next.d.ts
 import type { Metadata } from 'next';
 
-declare module 'next' {
+declare module 'next/types/next' {
   interface PageProps<P = {}> {
     params: P;
     searchParams?: { [key: string]: string | string[] | undefined };
