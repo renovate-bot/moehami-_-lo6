d1615c2dede1858f6b7e8ff0fdffadc291d239b6
moehami <m.aljaaly@yahoo.co.uk>
2024-12-14 06:33:04 +0200

    Create ClientBlogPost.tsx

---
+++ app/blog/[slug]/ClientBlogPost.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export default function ClientBlogPost({ post }: { post: any }) {
+  return (
+    <div className="container mx-auto px-4 py-12 prose prose-lg">
+      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+      <p className="mt-4 text-muted-foreground">
+        Published on: {new Date(post.date).toLocaleDateString()} By {post.author}
+      </p>
+      <article className="prose mt-4">
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+      </article>
+    </div>
+  );
+}
