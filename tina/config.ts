b4622573bd08f7d84c0ac4c9aed3ca79bc98920b
moehami <m.aljaaly@yahoo.co.uk>
2024-12-19 11:28:14 +0200

    Updated

--- tina/config.ts
+++ tina/config.ts
@@ -1,34 +1,30 @@
 import { defineConfig } from "tinacms";
 import slugify from "slugify"; // For generating SEO-friendly filenames
 
-
 // Your hosting provider likely exposes this as an environment variable
 const branch =
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  'main'
+  "main";
 
 export default defineConfig({
   branch,
-  apiUrl: process.env.NEXT_PUBLIC_APIURL || "http://localhost:4001/graphql",
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
-  
+  client: {
+    apiUrl: process.env.NEXT_PUBLIC_APIURL || "http://localhost:4001/graphql",
+    clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+    token: process.env.TINA_TOKEN, // Get this from tina.io
+  },
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
-
   media: {
     tina: {
       mediaRoot: "",
       publicFolder: "public",
     },
   },
-
   schema: {
     collections: [
       {
