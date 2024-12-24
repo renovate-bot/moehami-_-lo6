121eed8d96ccc140f0940534eacbd8b99d5f8d19
moehami <m.aljaaly@yahoo.co.uk>
2024-12-24 20:20:02 +0200

    Update By MOe Netlify CLI

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,39 @@ export const metadata: Metadata = {
     default: 'Lo Bin Stores',
   },
   description: 'Discover local bin stores near me selling a wide variety of trash cans, recycling bins, compost bins, and more. Find the perfect bin for your home or business.',
+  icons: {
+    icon: [
+      {
+        url: "/favicon/favicon.ico",
+        type: "image/x-icon"
+      },
+      {
+        url: "/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png"
+      }
+      // add favicon-32x32.png, favicon-96x96.png, android-chrome-192x192.png
+    ],
+    shortcut: [
+      {
+        url: "/favicon/favicon.ico",
+        type: "image/x-icon"
+      }
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-icon-57x57.png",
+        sizes: "57x57",
+        type: "image/png"
+      },
+      {
+        url: "/favicon/apple-icon-60x60.png",
+        sizes: "60x60",
+        type: "image/png"
+      }
+      // add apple-icon-72x72.png, apple-icon-76x76.png, apple-icon-114x114.png, apple-icon-120x120.png, apple-icon-144x144.png, apple-icon-152x152.png, apple-icon-180x180.png
+    ]
+  }
   openGraph: {
     type: 'website',
     locale: 'en_US',
