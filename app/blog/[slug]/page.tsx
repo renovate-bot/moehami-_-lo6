db916a2cda473b52873bff250be56076b11def1c
moehami <m.aljaaly@yahoo.co.uk>
2025-06-03 15:43:57 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -17,6 +17,7 @@ interface PostMetadata {
   description?: string;
   date: string;
   author: string;
+  image: string;
   metaDescription: string;
 }
 
@@ -70,39 +71,60 @@ export default async function BlogPost({ params }: { params: Promise<{ slug: str
   return (
 
       <div className="container mx-auto px-4 py-20">
-        <h1 className="text-4xl font-bold mb-8">{post.data.title}</h1>
-        <p className="mt-4 text-muted-foreground py-10">
-          Published on: {new Date(post.data.date).toLocaleDateString()}
-        </p>
+        <h1 className="text-4xl font-bold">{post.data.title}</h1>
 
-        <div className="text-sm leading-6">
-          <figure className="relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
-            <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
-              <p>Crafting engaging narratives that spotlight the unique shopping experience of bin stores.</p>
-            </blockquote>
-            <figcaption className="flex items-center space-x-4">
-              <Image
-                src={`/authors/${post.data.author}.jpg`}
-                alt={`Profile image for ${post.data.author}`}
-                width={403}
-                height={416}
-                className="flex-none w-14 h-14 rounded-full object-cover"
-                loading="lazy"
-                decoding="async"
+   <div className="flex flex-col sm:flex-row sm:items-center sm:gap-6 sm:py-4 rounded-lg">
+
+       <Image
+                priority={true}
+src={`/authors/${post.data.author}.jpg`} 
+alt={`${post.data.title}`}
+className='w-10 lg:max-w-2l mx-auto h-91 rounded-full sm:mx-0 sm:shrink-0'
+              /> 
+          <div className="space-y-1 text-left sm:text-left">
+    <div className="">
+      <span className="text-base">Author: </span><span className="text-base text-blue-600">{post.data.author}</span>
+      <span className="text-base"> | Published </span>   <span className="text-base text-muted-foreground">{new Date(post.data.date).toLocaleDateString()}</span>
+
+    </div>
+
+  </div>
+</div>
+     
+
+        <article className="prose prose-lg max-w-none prose-headings:font-bold prose-headings:mb-2 prose-p:mb-2 prose-strong:font-bold prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline">
+
+            <div className='relative max-w-4xl lg:max-w-6xl mx-auto'>
+                <Image
+                priority={true}
+                src={`${post.data.image}`}
+alt={`${post.data.title}`}
+                width={500}
+                height={500}
+                className='relative z-10 mx-auto block rounded-lg w-full h-auto opacity-100'
+                style={{ maxWidth: '90vw' }}
               />
-              <div className="flex-auto">
-                <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
-                  {post.data.author}
-                </div>
-                <div className="mt-0.5 dark:text-slate-300">
-                  Writer
-                </div>
               </div>
-            </figcaption>
-          </figure>
-        </div>
-        <article className="prose prose-lg max-w-none prose-headings:font-bold prose-headings:mb-4 prose-p:mb-4 prose-strong:font-bold prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown>
+   <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 rounded-lg shadow-lg outline outline-black/5">
+
+       <Image
+                priority={true}
+src={`/authors/${post.data.author}.jpg`} 
+alt={`${post.data.title}`}
+className='w-15 lg:max-w-2l mx-auto h-91 rounded-full sm:mx-0 sm:shrink-0'
+              /> 
+          <div className="space-y-2 text-center sm:text-left">
+    <div className="space-y-0.5">
+      <span className="text-lg font-semibold text-black">About the author: </span><p className="text-xl font-semibold text-blue-600">{post.data.author}</p>
+
+      <p className="font-medium text-gray-500">Crafting engaging narratives that spotlight the unique shopping experience of bin stores.</p>
+    </div>
+
+  </div>
+</div>
+
+          
         </article>
       </div>
 
